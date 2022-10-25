50ae94ca3536875d6934d81cc3ec60160e4fb6d4
Danny Collier <dcollie2@gmu.edu>
2022-10-25 18:13:21 -0400

    Begin adding CRUD.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :trackable, :validatable, :timeoutable
+
+  validates :email, presence: true
+
+  def set_default_role
+    self.role ||= :user
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users
   resources :systems
   resources :sites
   resources :scans do
