276b3c931fcef0d7803e0f36eea534634e42634f
Danny Collier <dcollie2@gmu.edu>
2022-10-25 12:22:50 -0400

    Add roll-up scores to systems.

--- app/models/system.rb
+++ app/models/system.rb
@@ -1,4 +1,24 @@
 class System < ApplicationRecord
   has_many :sites, dependent: :nullify
   has_many :scans, through: :sites
+
+  def current_pages
+    scans.most_recent.sum(:pages)
+  end
+
+  def current_overall_score
+    scans.most_recent.average(:overall_score).round(2)
+  end
+
+  def current_broken_links
+    scans.most_recent.sum(:broken_links)
+  end
+
+  def current_accessibility_issues
+    scans.most_recent.sum(:accessibility_issues)
+  end
+
+  def current_misspellings
+    scans.most_recent.sum(:misspellings)
+  end
 end
